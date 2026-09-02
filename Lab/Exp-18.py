2b3b44800e7c290196dc4fe14caf59bceb74f603
chola192325021 <154490177+chola192325021@users.noreply.github.com>
2026-09-02 10:09:30 +0530

    Create Exp-18.py

---
+++ Lab/Exp-18.py
@@ -0,0 +1,39 @@
+#exp-18
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+
+# Load Iris dataset
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Perceptron model
+model = Perceptron(max_iter=1000, random_state=42)
+
+# Train
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+print("Perceptron Based IRIS Classification")
+print("------------------------------------")
+
+print("Accuracy:",
+      accuracy_score(y_test, y_pred) * 100, "%")
+
+# New flower
+new_flower = [[5.1, 3.5, 1.4, 0.2]]
+
+prediction = model.predict(new_flower)
+
+print("Predicted Flower:",
+      iris.target_names[prediction[0]])
